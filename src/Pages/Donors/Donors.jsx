5591810f55529f4054c9573777a833ced3e23a7a
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-08 14:42:32 +0600

    added blood group and email to donors card

--- src/Pages/Donors/Donors.jsx
+++ src/Pages/Donors/Donors.jsx
@@ -31,7 +31,7 @@ const Donors = () => {
           <h1 className="text-2xl font-semibold mb-4 text-center">
             All Donors
           </h1>
-          <div className="grid grid-cols-1 gap-12">
+          <div className="grid grid-cols-1 md:grid-cols-2` gap-12">
             {donors.map((donor) => (
               <DonorsCard
                 key={donor._id}
--- src/Pages/Donors/DonorsCard.jsx
+++ src/Pages/Donors/DonorsCard.jsx
@@ -1,14 +1,16 @@
 import Swal from 'sweetalert2';
 
 const DonorsCard = ({ donor, donors }) => {
-  const { _id, name, photoURL } = donor;
+  const { _id, name, photoURL, bloodGroup, email } = donor;
 
   return (
-    <div className="card w-96 bg-base-100 shadow-xl p-5">
+    <div className="card w-full bg-base-100 shadow-xl p-5">
       <div className="">
         <div className="flex justify-between my-2 items-center">
           <img src={photoURL} className="w-20 rounded-full" alt="gadget" />
           <h2 className="text-lg font-semibold ">{name}</h2>
+          <h2 className="text-lg font-semibold ">{bloodGroup}</h2>
+          <h2 className="text-lg font-semibold ">{email}</h2>
         </div>
       </div>
     </div>
